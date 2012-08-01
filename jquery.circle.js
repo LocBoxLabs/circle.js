7ea6a33101a1eab4230f03ffb250cbfeece0314d
Colin Young <me@colinyoung.com>
2012-08-01 18:09:42 -0500

    Setup

---
+++ jquery.circle.js
@@ -0,0 +1,20 @@
+/**
+ * jquery.circle.js
+ * Copyright (c) 2012 Colin Young
+ * Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * 
+ * @author Colin Young
+ * @projectDescription	jQuery plugin for detecting the position of the mouse in radians on any HTML element
+ * @version 0.1.0
+ * 
+ * @requires jquery.js (tested with 1.2.6)
+ * 
+ */
+ 
+$(function() {
+
+	$.fn.circle = function ( options )
+	{ 
+	  
+  }
+});
