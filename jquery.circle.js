d9c5cb7a29e4bef09aa3a7e300c43145fb511571
Colin Young <me@colinyoung.com>
2012-08-01 20:17:54 -0500

    Tracking accurate position

--- jquery.circle.js
+++ jquery.circle.js
@@ -10,11 +10,36 @@
  * @requires jquery.js (tested with 1.2.6)
  * 
  */
- 
 $(function() {
 
 	$.fn.circle = function ( options )
 	{ 
+	  var _this = this, events = {};
+	  events.click = $.isFunction(options.click) ? options.click : null;
+	  events.mousemove = $.isFunction(options.mousemove) ? options.mousemove : null;
+	  this.events = events;
 	  
+	  /* Functions */
+	  this.util = {
+	    eventInfo: function(e) {
+	      return {
+	        x: Math.round(e.pageX - $(_this).offset().left, 0),
+	        y: Math.round(e.pageY - $(_this).offset().top, 0)
+	      };
+	    }
+    };
+	  
+	  $(this).mousemove(function(e) {
+	    var mousemove = _this.events.mousemove;
+	    if (!mousemove) return;
+	    
+	    mousemove.call(_this, _this.util.eventInfo(e));
+	  });
+	  
+	  $(this).click(function(e) {
+	    var click = _this.events.click;
+	    if (!click) return;
+	    click.call(_this, _this.util.eventInfo(e));
+	  });
   }
 });
