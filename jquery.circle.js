ed5c753b8b0fe5f3d9b3ecfff4136f6f596986a1
Colin Young <me@colinyoung.com>
2012-08-02 15:05:23 -0500

    Finish position.
    Percentage fonction courtesy of @jakechen.

--- jquery.circle.js
+++ jquery.circle.js
@@ -15,16 +15,43 @@ $(function() {
 	$.fn.circle = function ( options )
 	{ 
 	  var _this = this, events = {};
+	  this.options = $.extend({
+	    bounds: null
+	  }, options);
 	  events.click = $.isFunction(options.click) ? options.click : null;
 	  events.mousemove = $.isFunction(options.mousemove) ? options.mousemove : null;
 	  this.events = events;
 	  
 	  /* Functions */
 	  this.util = {
+	    // Special thanks to @jakechen.
+	    percentage: function(x, y) {
+	      var bounds = _this.options.bounds,
+	               d = $(_this).width(),
+	           theta = 0,
+	               v = 0;
+	      theta = Math.atan2((d-2*x), (2*y-d));
+	      if (x < (d/2)) {
+	        v = (theta/Math.PI) * 50;
+	      } else {
+	        v = 100 + (theta/Math.PI) * 50;
+	      }
+	      if ($.isArray(bounds)) {
+	        v = Math.max(v, bounds[0]);
+	        v = Math.min(v, bounds[1]);
+	      }
+	      return percentage = parseFloat(v.toFixed(2));
+	    },
 	    eventInfo: function(e) {
+	      var x, y, percentage;
+	      x = Math.round(e.pageX - $(_this).offset().left, 0);
+	      y = Math.round(e.pageY - $(_this).offset().top, 0);
+	      percentage = _this.util.percentage(x, y);
 	      return {
-	        x: Math.round(e.pageX - $(_this).offset().left, 0),
-	        y: Math.round(e.pageY - $(_this).offset().top, 0)
+	        x: x,
+	        y: y,
+	        percentage: percentage,
+	        degree: percentage/100 * 360
 	      };
 	    }
     };
